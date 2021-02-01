a1abd5b12fce56208e31a04ce300ebe7177e1984
nvalerio12 <nelson.j.valerio@gmail.com>
2021-02-01 14:17:26 -0500

    Added a momment

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-const myModule = require('./myModule')
+const fs = require('fs');
+const { request } = require('http');
+const moment = require('moment');
 
-const myOtherModule = require('./myOtherModule');
+let myData = moment('2021-02-01');
+let addOneWeek = myData.add (1, 'week');
+console.log(myData.format())
+fs.readFile('nfl.txt', 'utf8', function(error, data) {
+    if (error) {
+        console.log('You have a problem reading this file.')
+    } else {
+        console.log(data)
+    }
+    
+})
 
-const myOtherOtherModule = require('./myOtherOtherModule')
-console.log('Hello World')
 
-function hello(name) {
-    console.log(`Hello, ${name}`);
-}
 
-hello('Mocha');
-hello('JC');
 
-console.log(myModule.add(2, 3));
-console.log(myModule.subtract(5, 4));
 
-//my other module
-myOtherModule.welcomeToNode();
 
-console.log(myOtherOtherModule.divied(3, 21))
+
+
+
+
+
+
+
+
+
+
+// const myModule = require('./myModule')
+
+// const myOtherModule = require('./myOtherModule');
+
+// const myOtherOtherModule = require('./myOtherOtherModule')
+// console.log('Hello World')
+
+// function hello(name) {
+//     console.log(`Hello, ${name}`);
+// }
+
+// hello('Mocha');
+// hello('JC');
+
+// console.log(myModule.add(2, 3));
+// console.log(myModule.subtract(5, 4));
+
+// //my other module
+// myOtherModule.welcomeToNode();
+
+// console.log(myOtherOtherModule.divied(3, 21))
